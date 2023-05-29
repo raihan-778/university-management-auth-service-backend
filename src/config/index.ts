edcb768a284966dfbbdba77f6f1f1ca8790f79e8
Md.Raihan Uddin <raihanarif778@gmail.com>
2023-05-30 00:28:16 +0600

    eslint & prettier configur

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,8 @@
-import dotenv from "dotenv";
-import path from "path";
+import dotenv from 'dotenv'
+import path from 'path'
 
-dotenv.config({ path: path.join(process.cwd(), ".env") }); //hera "{ path: path.join(process.cwd(), ".env") }" is used to defined the default path for env file and join it with current working directory
+dotenv.config({ path: path.join(process.cwd(), '.env') }) //hera "{ path: path.join(process.cwd(), ".env") }" is used to defined the default path for env file and join it with current working directory
 export default {
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
-};
+}
