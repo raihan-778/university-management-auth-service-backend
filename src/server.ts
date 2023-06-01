5259da1d85e8503b0a868aceca17ed2f84bb953c
Md.Raihan Uddin <raihanarif778@gmail.com>
2023-06-02 00:31:50 +0600

    logger npm package added for creting log file

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config'
+import logger from './shared/logger'
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log(`🛢 Database connected successfully`)
+    logger.info(`🛢 Database connected successfully`)
     app.listen(config.port, () => {
-      console.log(`Application is listening on port ${config.port}`)
+      logger.info(`Application is listening on port ${config.port}`)
     })
   } catch (err) {
-    console.log(`Failed to connect database`, err)
+    logger.error(`Failed to connect database`, err)
   }
 }
 bootstrap()
