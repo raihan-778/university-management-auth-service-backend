0e8b0c07e2076404d3698556a9a32cc09ea833a8
Md.Raihan Uddin <raihanarif778@gmail.com>
2023-06-06 00:09:31 +0600

    global error handler function bug fixed

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,28 +1,55 @@
-import { NextFunction, Request, Response } from 'express'
+import { ErrorRequestHandler } from 'express'
 import config from '../../config'
+import ApiError from '../../errors/ApiError'
+import handleValidationError from '../../errors/handleValidationError'
 import { IGenericErrorMessage } from '../../interfaces/error'
 
-const globalErrorHandler = (
-  err,
-  req: Request,
-  res: Response,
-  next: NextFunction
+const globalErrorHandler: ErrorRequestHandler = (
+  // if any request handler has first parameter as err it will be a global error handeler of express, which can from ErrorRequestHandler
+  error,
+  req,
+  res,
+  next
 ) => {
-  const statusCode = 500
-  const message = 'Something went wrong!!'
-  const errorMessages: IGenericErrorMessage[] = []
+  let statusCode = 500
+  let message = 'Something went wrong!!'
+  let errorMessages: IGenericErrorMessage[] = []
 
-  //   if (err.name === 'ValidatorError') {
-  //     const simplifiedError = handleValidationError(err)
-  //   }
+  if (error.name === 'ValidatorError') {
+    const simplifiedError = handleValidationError(error)
+    statusCode = simplifiedError.statusCode
+    message = simplifiedError.message
+    errorMessages = simplifiedError.errorMessages
+  } else if (error instanceof ApiError) {
+    statusCode = error?.statusCode
+    message = error?.message
+    errorMessages = error?.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : []
+  } else if (error instanceof Error) {
+    message = error?.message
+    errorMessages = error?.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : []
+  }
 
   res.status(statusCode).json({
     success: false,
     message,
     errorMessages,
-    stack: config.env != 'production' ? err?.stack : undefined,
+    stack: config.env != 'production' ? error?.stack : undefined,
   })
 
-  next(err)
+  next(error)
 }
 export default globalErrorHandler
