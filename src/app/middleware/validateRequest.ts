de15b0a62b1a0006d2421ad04358bb8642c72575
Md.Raihan Uddin <raihanarif778@gmail.com>
2023-06-06 20:09:39 +0600

    Zod validation package added

---
+++ src/app/middleware/validateRequest.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from 'express'
+import { AnyZodObject } from 'zod'
+
+const validateRequest =
+  (schema: AnyZodObject) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema.parseAsync({
+        body: req.body,
+        query: req.query,
+        params: req.params,
+        cookies: req.cookies,
+      })
+      return next()
+    } catch (error) {
+      next(error)
+    }
+  }
+
+export default validateRequest
+
+//middleware--> (here validateRequest is a middleware which will take "userZodSchema" as a parameter) and return an async function which include req, res and next as parameters.
