7769242e285b38bad0c0e58cc28fbd9267b58831
Md.Raihan Uddin <raihanarif778@gmail.com>
2023-06-20 00:46:15 +0600

    dynamic Student & Faculty Id create function added

--- src/app/modules/user/user.router.ts
+++ src/app/modules/user/user.router.ts
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post(
   '/create-user',
   validateRequest(UserValidation.createUserZodSchema),
-  UserController.createUserToDB
+  UserController.createUser
 );
 
 export const UserRoutes = router;
--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -1,22 +1,53 @@
+import { IAcademicSemester } from '../academicSemester/academicSemester.interface';
+
 import { User } from './user.model';
 
-export const findLastUserId = async () => {
-  const lastUser = await User.findOne({}, { id: 1, _id: 0 })
+export const findLastStudentId = async () => {
+  const lastStudent = await User.findOne({ role: 'Student' }, { id: 1, _id: 0 })
     .sort({
       createdAt: -1,
     })
     .lean();
-  return lastUser?.id;
+  return lastStudent?.id ? lastStudent.id.substring(4) : undefined;
 };
 
-export const generateUserId = async () => {
-  const currentUserId =
-    (await findLastUserId()) || (0).toString().padStart(5, '0');
+export const generateStudentId = async (
+  academicSemester: IAcademicSemester
+) => {
+  const currentStudentId =
+    (await findLastStudentId()) || (0).toString().padStart(5, '0');
 
   //increment id by 1
-  const newIncrementId = (parseInt(currentUserId) + 1)
+  let newIncrementId = (parseInt(currentStudentId) + 1)
     .toString()
     .padStart(5, '0');
 
+  newIncrementId = `${academicSemester.year.substring(2)}${
+    academicSemester.code
+  }${newIncrementId}`;
+
   return newIncrementId;
+};
+
+const findLastFacultyId = async () => {
+  const lastFaculty = await User.findOne(
+    { role: 'Faculty' },
+    { id: 1, _id: -1 }
+  )
+    .sort({ createdAt: -1 })
+    .lean();
+  return lastFaculty?.id;
+};
+
+export const generateFacultyId = async () => {
+  const currentFacultyId =
+    (await findLastFacultyId()) || (0).toString().padStart(5, '0');
+
+  let incrementedFacultyId = (parseInt(currentFacultyId) + 1)
+    .toString()
+    .padStart(5, '0');
+
+  incrementedFacultyId = `F-${incrementedFacultyId}`;
+  // return incrementedFacultyId;
+  console.log(incrementedFacultyId);
 };
