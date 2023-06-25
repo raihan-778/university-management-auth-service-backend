3798661416fb444050edde8aa33a42bf404520db
raihan-778 <raihanarif778@gmail.com>
2023-06-25 17:50:12 +0600

    .env file updated

---
+++ .env.example.ts
@@ -0,0 +1,14 @@
+NODE_ENV=development
+PORT=5000
+
+# password=@user
+DATABASE_URL=mongodb+srv://wds-raihan:swUwDvZ46qma1uFB@cluster0.safuyuz.mongodb.net/university-management?retryWrites=true&w=majority
+
+DEFAULT_USER_PASS=university123.@$
+DEFAULT_STUDENT_PASS=phuStd$@&%
+DEFAULT_FACULTY_PASS=phuflt$@&%
+BCRYPT_SALT_ROUND=12
+JWT_SECRET=jwtVerySecret
+JWT_EXPIRES_IN=1d
+JWT_REFRESH_SECRET=jwtRefreshSecretImp
+JWT_REFRESH_EXPERES_IN=365d
